23373a70b8e2acc3e16c66d4f11c0a3da44a37db
evolvedmicrobe_cp <SND\evolvedmicrobe_cp>
2013-06-25 13:30:38 -0700

    Fixed unit test, now two tests where one sequence "fails" to be assembled and looks like the first.

--- bio/Tests/Bio.Tests.Padena/ParallelDeNovoAssemblerTests.cs
+++ bio/Tests/Bio.Tests.Padena/ParallelDeNovoAssemblerTests.cs
@@ -57,9 +57,9 @@ namespace Bio.Tests
         }
         /// <summary>
         /// Tests a quasi-palindromic sequence, where an 19 bp kmer overlaps at 18 bp with the 
-        /// reverse complement of itself, forming a chain, but not a loop. A bad algorithm may not be able 
-        /// to reconstruct this.
-        /// 
+        /// reverse complement of itself.  This is an interesting case because it forms a loop, but the path out of the node 
+        /// is not the same as before.  In deciding how to handle this, one of the two test cases must be incorrectly assembled, as 
+        /// occurs in this test with the current simple path algorithm.
         /// </summary>
         [TestMethod]
         [Priority(0)]
@@ -67,28 +67,37 @@ namespace Bio.Tests
         public void PalindromicAssembleTest()
         {
             const int KmerLength = 19;
-            string testSeq = @"GGTTTTTTCAATTGAAAAAAATCTGTATT";
+            string testSeq = @"TTTTTTCAATTGAAAAAAATCTGTATT";
+            string testSeq2 = "T" + testSeq;
             var testSequence = new Sequence(DnaAlphabet.Instance, testSeq);
+            var testSequence2 = new Sequence(DnaAlphabet.Instance, testSeq2);
             List<ISequence> seqs = new List<ISequence>();
-            seqs.Add(testSequence);
-            using (ParallelDeNovoAssembler assembler = new ParallelDeNovoAssembler())
+            
+            //two test sequences that are different but assemble to the same sequence
+            //only one of these can be done correctly in current algorithmic setup
+            //using simple paths, that must be the first one.
+            foreach (var curTestSeq in new[] { testSequence, testSequence2 })
             {
-                assembler.KmerLength = KmerLength;
-                assembler.AllowErosion = false;
-                assembler.AllowLowCoverageContigRemoval = false;
-                assembler.ContigCoverageThreshold = 0;
-                assembler.DanglingLinksThreshold = 0;
-                
-                IDeNovoAssembly result = assembler.Assemble(seqs);
+                seqs.Clear();
+                seqs.Add(curTestSeq);
+                using (ParallelDeNovoAssembler assembler = new ParallelDeNovoAssembler())
+                {
+                    assembler.KmerLength = KmerLength;
+                    assembler.AllowErosion = false;
+                    assembler.AllowLowCoverageContigRemoval = false;
+                    assembler.ContigCoverageThreshold = 0;
+                    assembler.DanglingLinksThreshold = 0;
 
-                // Compare the two graphs
-                Assert.IsTrue(result.AssembledSequences.Count == 1);
-                Assert.AreEqual(1, result.AssembledSequences.Count());
-                bool correctContig = result.AssembledSequences[0].SequenceEqual(testSequence);
-                if (!correctContig)
-                    correctContig = result.AssembledSequences[0].GetReverseComplementedSequence().Equals(testSequence);
-                Assert.IsTrue(correctContig);
+                    IDeNovoAssembly result = assembler.Assemble(seqs);
+                    // Compare the two graphs, ensure that an additional base is not added (which might be inco
+                    Assert.AreEqual(1, result.AssembledSequences.Count);
+                    bool correctContig = result.AssembledSequences[0].SequenceEqual(testSequence);
+                    if (!correctContig)
+                        correctContig = result.AssembledSequences[0].GetReverseComplementedSequence().SequenceEqual(testSequence);
+                    Assert.IsTrue(correctContig);
+                }
             }
+            
         }
     }
 }
