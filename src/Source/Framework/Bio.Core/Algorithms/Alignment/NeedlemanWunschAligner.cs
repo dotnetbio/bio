d3219c939013e2b15513dabd6f133aa45ff67164
Nigel Delaney <spamavoid@outlook.com>
2015-12-06 15:27:58 -0800

    Less Memory Usage / More Comments in NeedlemanWunschAligner
    For these dynamic programming problems, when traversing through the
    matrix one must store the entire traceback matrix,
    but the scores are only needed for the current row and the row above it.
    
    In the current implementation, we only use two rows for the "top
    scoring" (and match) matrix, but fill out entire matrices for the
    deletion and insert (or vertical and horizontal) matrices.  This is
    inefficient, as we then need 2 x M x N memory rather than 2 x 2 x N
    memory.  I switched this to only use the smaller amount.  Additionally:
    
    * Changed the minimum score on the edges from an arbitray low number to a constant near Int32.MinValue for clarity.
    
    * Added more comments throughout.
    
    * Rather than have three arrays, I used 1 array of a struct which may help memory locality for very large global alignments.
    
    * Added more unit tests that directly compare against EMBOSS Needle alignment program, found and reported a bug in NEEDLE while doing that...
    
    * Change all the vague category "Priority2" labels in NW tests to a newer version.

--- src/Source/Framework/Bio.Core/Algorithms/Alignment/NeedlemanWunschAligner.cs
+++ src/Source/Framework/Bio.Core/Algorithms/Alignment/NeedlemanWunschAligner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Bio.Algorithms.Alignment
 {
@@ -30,6 +31,40 @@ namespace Bio.Algorithms.Alignment
         public override string Description
         {
             get { return Properties.Resource.NEEDLEMAN_DESCRIPTION; }
+        }
+
+        /// <summary>
+        /// A element holding the match and gap scores, 
+        /// allows us to store three matrices in one.
+        /// </summary>
+        internal struct ScoreElement {
+            /// <summary>
+            /// The score for a diagonal move.
+            /// </summary>
+            internal int MatchScore;
+            /// <summary>
+            /// The score for a horizontal move.
+            /// </summary>
+            internal int HorizontalGapScore;
+            /// <summary>
+            /// The score for a vertical move.
+            /// </summary>
+            internal int VerticalGapScore;
+
+            /// <summary>
+            /// Gets the best score for this cell, used to compute the match score 
+            /// which is the max of (A + MatchScore, B + MatchScore, C + VerticalGapScore).
+            /// Since the max was already computed earlier to determine the traceback,
+            /// in theory we can store that value rather than recompute the max, but
+            /// this is expected to be fast enough.
+            /// </summary>
+            /// <value>The best score.</value>
+            internal int BestScore {
+                get 
+                {
+                    return Math.Max (Math.Max (MatchScore, HorizontalGapScore), VerticalGapScore);
+                }
+            }
         }
 
         /// <summary>
@@ -140,69 +175,94 @@ namespace Bio.Algorithms.Alignment
         /// </summary>
         private IEnumerable<OptScoreMatrixCell> CreateAffineTracebackTable()
         {
+            /* We will need a low constant number that is not Int32.MinValue 
+             * for the boundaries of the matrix (if we used Int32.MinValue it would 
+             * over flow when added to the move score to a very high value and become the max
+             * rather than the min).
+             */
+            int EDGE_MIN_VALUE = Int32.MinValue + Math.Max(Rows, Cols) * Math.Abs (GapOpenCost);
+
+
             // Horizontal and vertical gap counts.
             int gapStride = Cols + 1;
+
+            /* These are equivalent to traceback matrices for the 
+             * horizontal and vertical "gap" matrices.  However, rather than store
+             * an arrow that points in a direction, we store the size of moves,
+             * which allows us to transition into and out of the gap matrices in one
+             * step, rather than following paths through them.
+             */
             h_Gap_Length = new int[(Rows + 1) * gapStride];
             v_Gap_Length = new int[(Rows + 1) * gapStride];
-            int[] hgapCost = new int[(Rows + 1) * gapStride];
-            int[] vgapCost = new int[(Rows + 1) * gapStride];
 
-            int[] scoreLastRow = new int[Cols];
-            int[] scoreRow = new int[Cols];
+            /* As we progress through, we only need to know about 
+             * the current row and the previous row for the recursions,
+             * so we only use these two, rather than a full matrix.
+             */
+            ScoreElement[] previousScoreRow = new ScoreElement[Cols];
+            ScoreElement[] currentScoreRow = new ScoreElement[Cols];
             int[][] matrix = SimilarityMatrix.Matrix;
 
-            // Initialize the gap extension cost matrices.
-            // Since this is a global alignment, the first column and first row should be set to the 
-            // indels to get there
-            for (int i = 1; i < Rows; i++)
-            {
-                //can't move horizontally at this stage, set to value 
-                //where gap open is destined to be better.
-                sbyte[] traceback = new sbyte[Cols];
-                traceback [0] = SourceDirection.Up;
-                Traceback [i] = traceback;
-                hgapCost[i * gapStride] = 2*GapOpenCost*i;
-                v_Gap_Length[i * gapStride] = i;
-                int initialScore = (i - 1) * GapExtensionCost + GapOpenCost;
-                if (IncludeScoreTable)
-                    ScoreTable[i * gapStride] = initialScore;
+            // Upper left element is initalized to 0
+            currentScoreRow [0] = new ScoreElement () {
+                HorizontalGapScore = EDGE_MIN_VALUE,
+                VerticalGapScore = EDGE_MIN_VALUE,
+                MatchScore = 0
+            };
+            if (IncludeScoreTable) {
+                ScoreTable[0] = 0;
+
             }
-            scoreRow[0] = GapOpenCost;
-            for (int j = 1; j < Cols; j++)
+            // Initialize first row
+            sbyte[] traceback = Traceback [0];
+            for (int j = 1; j < traceback.Length; j++)
             {
-                //always have to go left from top
-                Traceback[0][j] = SourceDirection.Left;
+                // always have to go left from top
+                traceback[j] = SourceDirection.Left;
                 h_Gap_Length[j] = j;
-                //make sure the gap extension option from the top row going vertically is not picked, as 
-                //nonsense would result (can't move further up along query)
-                vgapCost[j] = j * 2 * GapOpenCost;
-                int initialScore = (j - 1) * GapExtensionCost + GapOpenCost;
-                scoreLastRow[j] = initialScore;
+                var initialScore = (j - 1) * GapExtensionCost + GapOpenCost;
+                currentScoreRow [j] = new ScoreElement () {
+                    HorizontalGapScore = initialScore,
+                    VerticalGapScore = EDGE_MIN_VALUE,
+                    MatchScore = EDGE_MIN_VALUE
+                };
                 if (IncludeScoreTable)
-                    ScoreTable[j] = initialScore;
+                   ScoreTable[j] = initialScore;
             }
-            
+
             for (int i = 1; i < Rows; i++)
             {
                 // Create the next TB row
-                sbyte[] traceback = Traceback[i];
-                if (i > 1)
-                {
-                    // Move current row to last row, Array.Copy(scoreRow, scoreLastRow, Cols);
-                    var tmp = scoreLastRow;
-                    scoreLastRow = scoreRow;
-                    scoreRow = tmp;                    
-                    // Initialize the next scoring row, should be no need for this since we are 
-                    //doing it as run.
-                    scoreRow[0] = GapExtensionCost * (i-1)+GapOpenCost;
+                traceback = new sbyte[Cols];
+                Traceback [i] = traceback;
+                // Make the current row the last row,
+                // reuse the current rows memory for the new row
+                SwapArrays(ref currentScoreRow, ref previousScoreRow);
+              
+                /* Initialize first column
+                 * We can't move further back horizontally here (we are at 
+                 * the edge), so we set the score to something so low we will
+                 * never go this direction on the highest scoring path, and so 
+                 * can avoid explicitly checking for an edge
+                 */
+                currentScoreRow[0] = new ScoreElement() {
+                    MatchScore = EDGE_MIN_VALUE, 
+                    HorizontalGapScore = EDGE_MIN_VALUE, 
+                    VerticalGapScore = (i - 1) * GapExtensionCost + GapOpenCost};
+                traceback[0] = SourceDirection.Up;
+                v_Gap_Length[i * gapStride] = i;
 
-                }                
                 for (int j = 1; j < Cols; j++)
                 {
+                    // Get the three important values
+                    var cellAbove = previousScoreRow[j];
+                    var cellDiagAbove = previousScoreRow [j - 1];
+                    var cellLeft = currentScoreRow [j - 1];
+
                     // Gap in reference sequence
                     int scoreAbove;
-                    int scoreAboveOpen = scoreLastRow[j] + GapOpenCost;
-                    int scoreAboveExtend = vgapCost[(i-1) * gapStride + j] + GapExtensionCost;
+                    int scoreAboveOpen = cellAbove.BestScore + GapOpenCost;
+                    int scoreAboveExtend = cellAbove.VerticalGapScore + GapExtensionCost;
                     if (scoreAboveOpen > scoreAboveExtend)
                     {
                         scoreAbove = scoreAboveOpen;
@@ -216,8 +276,8 @@ namespace Bio.Algorithms.Alignment
 
                     // Gap in query sequence
                     int scoreLeft;
-                    int scoreLeftOpen = scoreRow[j-1] + GapOpenCost;
-                    int scoreLeftExtend = hgapCost[i * gapStride + (j-1)] + GapExtensionCost;
+                    int scoreLeftOpen = cellLeft.MatchScore + GapOpenCost;
+                    int scoreLeftExtend = cellLeft.HorizontalGapScore + GapExtensionCost;
                     if (scoreLeftOpen > scoreLeftExtend)
                     {
                         scoreLeft = scoreLeftOpen;
@@ -229,35 +289,36 @@ namespace Bio.Algorithms.Alignment
                         h_Gap_Length[i * gapStride + j] = h_Gap_Length[i * gapStride + (j - 1)] + 1;
                     }
 
-                    // Store the gaps costs for this cell
-                    hgapCost[i * gapStride + j] = scoreLeft;
-                    vgapCost[i * gapStride + j] = scoreAbove;
-
                     // Get the exact match/mismatch score
                     int mScore = (matrix != null) ? matrix[QuerySequence[i - 1]][ReferenceSequence[j - 1]] : SimilarityMatrix[QuerySequence[i - 1], ReferenceSequence[j - 1]];
-                    int scoreDiag = scoreLastRow[j - 1] + mScore;
+                    /* Since all possible previous states have the same match score applied, 
+                     * we can just add to the previous max */
+                    int scoreDiag = cellDiagAbove.BestScore + mScore;
+
+                    // Store the scores for this cell
+                    currentScoreRow[j] = new ScoreElement() { HorizontalGapScore = scoreLeft,
+                                                              VerticalGapScore = scoreAbove,
+                                                              MatchScore = scoreDiag };
+                                                              
 
                     // Get the max S = MAX(diag,above,left) and assign the traceback
                     if ((scoreDiag > scoreAbove) && (scoreDiag > scoreLeft))
                     {
-                        scoreRow[j] = scoreDiag;
                         traceback[j] = SourceDirection.Diagonal;
                     }
                     else if (scoreLeft > scoreAbove)
                     {
-                        scoreRow[j] = scoreLeft;
                         traceback[j] = SourceDirection.Left;
                     }
                     else //if (scoreAbove > scoreLeft)
                     {
-                        scoreRow[j] = scoreAbove;
                         traceback[j] = SourceDirection.Up;
                     }
                 }
 
                 if (IncludeScoreTable)
                 {
-                    Array.Copy(scoreRow, 0, ScoreTable, i * Cols, Cols);
+                    Array.Copy(currentScoreRow.Select(x => x.BestScore).ToArray(), 0, ScoreTable, i * Cols, Cols);
                 }
             }
 
@@ -267,10 +328,15 @@ namespace Bio.Algorithms.Alignment
                 {
                     Row = Rows-1, 
                     Col = Cols-1, 
-                    Score = scoreRow[Cols-1],
+                    Score = currentScoreRow[Cols-1].BestScore,
                 }
             };
         }
        
+        private void SwapArrays(ref ScoreElement[] previousRow, ref ScoreElement[] currentRow) {
+            var tmp = previousRow;
+            previousRow = currentRow;
+            currentRow = tmp;
+        }
     }
 }
