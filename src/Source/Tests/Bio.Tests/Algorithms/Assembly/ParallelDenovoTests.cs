3d790b9de22b8e1484ac36b04e0a2534631748ea
Nigel Delaney <spamavoid@outlook.com>
2016-05-03 03:09:06 -0700

    Add Test for SNP being removed

--- src/Source/Tests/Bio.Tests/Algorithms/Assembly/ParallelDenovoTests.cs
+++ src/Source/Tests/Bio.Tests/Algorithms/Assembly/ParallelDenovoTests.cs
@@ -45,9 +45,33 @@ namespace Bio.Tests.Algorithms.Assembly
             Assert.AreEqual (1, contigs.Count);
             var found_seq = (contigs.First ().GetReverseComplementedSequence() as Sequence).ConvertToString ();
             Assert.AreEqual (s1.ConvertToString (), found_seq);
-
         }
 
+        [Test]
+        [Category("Padena")]
+        public void ValidateSNPsRemoved()
+        {
+            var seq1 = "ACGTACCCGATAGACACGATACGACAACCCTTTCACGAATCGATACGCAGTACAGATA".Select (x => (byte)x).ToArray ();
+            var seq2 = "ACGTACCCGATAGACACGATACGACAACCCTATCACGAATCGATACGCAGTACAGATA".Where(z=> z != '-').Select (x => (byte)x).ToArray ();
+            List<Sequence> data = new List<Sequence> (1000);
+            var s1 = new Sequence (DnaAlphabet.Instance, seq1, false);
+            var s2 = new Sequence (DnaAlphabet.Instance, seq2, false);
+            foreach (var i in Enumerable.Range(0, 600)) {
+                data.Add (s1);
+            }
+            foreach (var i in Enumerable.Range(0, 400)) {
+                data.Add (s2);
+            }
+            ParallelDeNovoAssembler asm = new ParallelDeNovoAssembler ();
+            asm.KmerLength = 17;
+            asm.RedundantPathLengthThreshold = 50;
+            asm.ErosionThreshold = 0;
+            asm.DanglingLinksThreshold = 0;
+            var contigs = asm.Assemble (data).AssembledSequences.ToList();
+            Assert.AreEqual (1, contigs.Count);
+            var found_seq = (contigs.First ().GetReverseComplementedSequence() as Sequence).ConvertToString ();
+            Assert.AreEqual (s1.ConvertToString (), found_seq);
+        }
        
     }
 }
